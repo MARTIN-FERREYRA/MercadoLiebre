4114a805e38baf333786fb9756b023259affeed2
MARTIN-FERREYRA <ferreyramartinj@gmail.com>
2023-06-08 19:47:18 -0300

    primer entrega mercado libre

---
+++ README.md
@@ -0,0 +1,2 @@
+Proyecto mercado liebre para DigitalHouse, actividad con html y css desde cero
+
---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require ('express');
+const app = express();
+const path = require ('path');
+
+const publicFolderPath = path.resolve ('public');
+app.use(express.static(publicFolderPath));
+
+app.use (express.json());
+app.use (express.urlencoded());
+const port = process.env.PORT || 3030;
+app.listen (port, () => {
+    console.log('Servidor corriendo')
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve('views/home.html'))
+})
+
+app.get('/register', (req, res) => {
+    res.sendFile(path.resolve('views/register.html'))
+})
+
+app.get('/searchbar', (req, res) => {
+    res.sendFile(path.resolve('views/searchbar.html'))
+})
+
+
+app.post('/', (req, res) => {
+  return res.send (req.body);
+})
+
+app.get('/login', (req, res) => {
+    res.sendFile(path.resolve('views/login.html'))
+})
+; 
